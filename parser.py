a15e61e1b3e2d5cb6750fd1e5b0cff73f2cd5fa6
Ethan <ethan24@gmail.com>
2015-04-29 21:48:13 -0400

    Adding chapters to parser

--- parser.py
+++ parser.py
@@ -1,15 +1,14 @@
 __author__ = 'ecowan'
 
+import re
+
 
 class Parser:
 
     def __init__(self, file_name):
         self.text_string = open(file_name, 'r').read()
         self.lines = [x for x in self.text_string.split('\n') if len(x) > 0]
+        self.chapters = self.get_chapters()
 
     def get_chapters(self):
-        '''
-        Split on month names probably, from June to November
-        :return:
-        '''
-        pass
+        return [x for x in re.compile('^Progris|^PROGRESS', re.MULTILINE).split(self.text_string) if len(x) > 0]
